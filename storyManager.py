e979ba81a9f8523dd8ea1066e6d7eb7367013740
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-08-09 18:43:35 -0400

    Made all inventory things appear

--- storyManager.py
+++ storyManager.py
@@ -22,6 +22,11 @@ fish = task.Task(
     reward={1: {inv.lookup("Fish"): 1}}
 )
 
-document.querySelector("#wood-count").textContent = str(wood.quantity)
+for item in inv.Resource.all:
+    item_id = item.name.lower().replace(" ", "-")
+    resource_line = document.createElement("p")
+    resource_line.id = f"{item_id}-count"
+    resource_line.textContent = f"{item.name}: {item.quantity}"
+    document.querySelector("#inventory-col").appendChild(resource_line)
 
 
