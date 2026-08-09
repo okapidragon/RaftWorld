fcd4881256f7467f58ba0d2f0adbcd1a8781a74e
zacksloth5 <zackgourevitch@gmail.com>
2026-08-09 16:32:04 -0400

    Added Basic Inventory Logic, Resource Class, initialize, add, remove, convert to a string

--- InventoryLogic.py
+++ InventoryLogic.py
@@ -1,1 +1,19 @@
+class Resource:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
 
+    def add(self, amount):
+        self.quantity += amount
+
+    def remove(self, amount):
+        if amount <= self.quantity:
+            self.quantity -= amount
+            return True
+        else:
+            raise ValueError(f"Not enough {self.name} in inventory to remove {amount}. Current quantity: {self.quantity}")
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity}"
+
+    
