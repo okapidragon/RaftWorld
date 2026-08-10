2f5f2a9b09759cee313715a2c1a09b9030098699
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-08-10 10:16:19 -0400

    Moved inventory update

--- inventoryLogic.py
+++ inventoryLogic.py
@@ -1,11 +1,21 @@
 import imports as imp
 
+def inventoryUpdate():
+    for item in inv.Resource.all:
+        item_id = item.name.lower().replace(" ", "-")
+        resource_line = imp.document.createElement("p")
+        resource_line.id = f"{item_id}-count"
+        resource_line.textContent = f"{item.name}: {item.quantity}"
+        imp.document.querySelector("#inventory-col").appendChild(resource_line)
+
+
 class Resource:
     all = []
 
     def __init__(self, name, quantity):
         self.name = name
         self.quantity = quantity
+        inventoryUpdate()
         Resource.all.append(self)
 
     def add(self, amount):
