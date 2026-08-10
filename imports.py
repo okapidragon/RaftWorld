cfd6a47651a5da025ca32b001d039c4d86026139
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-08-10 10:12:18 -0400

    newest message are first

--- imports.py
+++ imports.py
@@ -9,7 +9,7 @@ def displayOutput():
 
     output_column.innerHTML = "<h3>Output</h3>"
 
-    for message in outputMessage.messages[-4:]:
+    for message in reversed(outputMessage.messages[-4:]):
         message_line = document.createElement("p")
         message_line.textContent = message
         output_column.appendChild(message_line)
