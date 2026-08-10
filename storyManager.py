2635040c8c8b40bd24e378272ee57fcd5f99db65
zacksloth5 <zackgourevitch@gmail.com>
2026-08-10 11:52:02 -0400

    stopped constant time prints

--- storyManager.py
+++ storyManager.py
@@ -39,7 +39,7 @@ async def timeLoop():
         gameTime.advance(3)
 
         timeText = f"{gameTime.hours:02d}:{gameTime.minutes:02d}"
-        imp.outputMessage.append(f"Time: {timeText}")
+        #imp.outputMessage.append(f"Time: {timeText}")
         await imp.asyncio.sleep(0.5)
 
 imp.asyncio.create_task(hungerLoop())
