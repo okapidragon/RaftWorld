51e47536768a404079beacff2804039cb2c65e46
zacksloth5 <zackgourevitch@gmail.com>
2026-08-10 12:16:36 -0400

    15 messages are displayed now

--- imports.py
+++ imports.py
@@ -10,7 +10,7 @@ def displayOutput():
 
     output_column.innerHTML = "<h3>Output</h3>"
 
-    for message in reversed(outputMessage.messages[-10:]):
+    for message in reversed(outputMessage.messages[-15:]):
         message_line = document.createElement("p")
         message_line.textContent = message
         output_column.appendChild(message_line)
