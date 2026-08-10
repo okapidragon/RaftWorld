2622b21d263490f6f0b667bd64ff2997396fb554
zacksloth5 <zackgourevitch@gmail.com>
2026-08-10 14:56:42 -0400

    days displayed diff

--- playerLogic.py
+++ playerLogic.py
@@ -84,7 +84,7 @@ class Player:
 
 
 def displayTime(current_time):
-    timeText = f"Day {current_time.days:02d} {current_time.hours:02d}:{current_time.minutes:02d}"
+    timeText = f"Day {current_time.days} {current_time.hours:02d}:{current_time.minutes:02d}"
     time_line = imp.document.querySelector("#time-display")
     time_line.textContent = f"Time: {timeText}"
 
