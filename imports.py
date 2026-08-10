cf83d2ff07837dc1416b681d7a291e19b4a9a45c
zacksloth5 <zackgourevitch@gmail.com>
2026-08-10 09:52:13 -0400

    change inports

--- imports.py
+++ imports.py
@@ -2,4 +2,13 @@ import random
 import threading
 import time
 
-outputMessage = []
+
+
+class OutputMessage:
+    def __init__(self):
+        self.messages = []
+
+    def append(self, message):
+        self.messages.append(message)
+
+outputMessage = OutputMessage()
