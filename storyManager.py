459a6cfe5376de0e3edc729c6daf4c90af55b57c
zacksloth5 <zackgourevitch@gmail.com>
2026-08-11 10:08:21 -0400

    Added items.py and moved tasks and resources there

--- storyManager.py
+++ storyManager.py
@@ -11,40 +11,9 @@ gameTime = pl.gameTime()
 
 pl.displayBoatSize(boat)
 
-imp.displayedResources.append(inv.Item("Fishing Rod", 1, breakable=True, breakChance=0.03))
-
 imp.outputMessage.append("You are stranded in the middle of a freshwater ocean with only a fishing rod and wooden raft to your name. You need to battle starvation (when the hunger bar reaches -100) by fishing and getting food.")
 
-inv.Resource("Fish", 0, food = True, hungerScore=100)
-inv.Resource("Seaweed", 0, food = True, hungerScore = 8)
-imp.displayedResources.append(inv.Resource("Wood", 0))
-
 inv.inventoryUpdate()
-
-
-
-imp.displayedTasks.append(task.Task(
-    name="Fishing",
-    cost={},
-    neededItems=[inv.lookup("Fishing Rod")],
-    time=6,
-    reward= {0.8: ( {}, "You could not catch a fish!"),
-            0.95: ( {inv.lookup("Fish"): 1}, "You caught a sardine"),
-            0.99: ( {inv.lookup("Fish"): 2}, "You caught a salmon"),
-            1.0: ( {inv.lookup("Fish"): 5}, "You caught a tuna")}
-))
-
-task.Task(name = "Gather Seaweed",
-        cost = {},
-        neededItems=[],
-        time = 2,
-        reward = {0.4: ({inv.lookup("Seaweed"): 0}, "You failed to gather seaweed"),
-            0.7: ({inv.lookup("Seaweed"): 1}, "You found one piece of seaweed"),
-            0.95: ({inv.lookup("Seaweed"): 3}, "You found a large blob of seaweed"),
-            1: ({inv.lookup("Seaweed"): 6}, "You found piles of seaweed") })
-
-
-
 
 task.taskButtonUpdate()
 
