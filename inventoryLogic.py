67718389b71dc2a00b6d6f6a5db72d22078dc8bd
zacksloth5 <zackgourevitch@gmail.com>
2026-08-11 14:12:46 -0400

    neatness fix

--- inventoryLogic.py
+++ inventoryLogic.py
@@ -28,8 +28,7 @@ class Resource:
         if food:
             Resource.allFood.append(self)
             self.hungerScore = hungerScore
-
-        self.eatQuantity = eatQuantity
+            self.eatQuantity = eatQuantity
 
     def add(self, amount):
         self.quantity += amount
