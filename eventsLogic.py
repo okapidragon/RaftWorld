63f8c8dbc129854e82790bd3ce0fd74d68a758a9
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-08-12 14:29:15 -0400

    Fixed boat centering and stuff

--- eventsLogic.py
+++ eventsLogic.py
@@ -165,7 +165,6 @@ def stopEvent(event):
     imp.currentEvent = None
 
     if not imp.boatUnlock:
-        imp.showSomething("#boat-col")
         imp.showSomething("#boat-div")
         imp.outputMessage.append("While pondering a way to get a paddle. You notice that you can take apart your raft, your only life supply. Be Careful!", color = "#50C878")
         imp.boatUnlock = True
