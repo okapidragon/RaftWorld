fa3a3fac1aab2f3e534da3860df7c360377abb3c
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-08-12 15:37:02 -0400

    Gradual display

--- playerLogic.py
+++ playerLogic.py
@@ -142,7 +142,7 @@ class Boat:
 
         if not (inv.lookup("Wood") in imp.displayedResources):
             imp.displayedResources.append(inv.lookup("Wood"))
-            task.craftButtonUpdate()
+            task.imp.asyncio.create_Task.craftButtonUpdate()
 
     def decayFrame(self):
         if imp.boatDecay:
