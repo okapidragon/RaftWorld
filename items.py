bfb0b73439cc0b130192e0ea44f203461faf80fb
zacksloth5 <zackgourevitch@gmail.com>
2026-08-12 09:17:19 -0400

    Fixed catastrophic bug

--- items.py
+++ items.py
@@ -25,7 +25,7 @@ time=7,
 reward= {0.8: ( {inv.lookup("Fish"): 0}, "You could not catch a fish!"),
         0.95: ( {inv.lookup("Fish"): 1}, "You caught a sardine"),
         0.99: ( {inv.lookup("Fish"): 2}, "You caught a salmon"),
-        1.0: ( {inv.lookup("Fish"): 5}, "You caught a tuna")}
+        1.0: ( {inv.lookup("Fish"): 5}, "You caught a tuna")},
 cutChance = 0.075
 ))  
 
@@ -37,7 +37,7 @@ task.Task(name = "Gather Seaweed",
     reward = {0.4: ({inv.lookup("Seaweed"): 0}, "You failed to gather seaweed"),
         0.7: ({inv.lookup("Seaweed"): 1}, "You found one piece of seaweed"),
         0.95: ({inv.lookup("Seaweed"): 3}, "You found a large blob of seaweed"),
-        1: ({inv.lookup("Seaweed"): 6}, "You found piles of seaweed") }
+        1: ({inv.lookup("Seaweed"): 6}, "You found piles of seaweed") },
     cutChance = 0.05
         )
 
