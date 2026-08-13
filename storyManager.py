e19bda553253bf98f3635b6a9271a3ac8715984c
zacksloth5 <zackgourevitch@gmail.com>
2026-08-13 11:07:08 -0400

    Deleted immidieta merchant appearance

--- storyManager.py
+++ storyManager.py
@@ -47,5 +47,3 @@ imp.asyncio.create_task(hungerLoop())
 imp.asyncio.create_task(timeLoop())
 imp.asyncio.create_task(eventLoop())
 imp.asyncio.create_task(decayLoop())
-
-imp.asyncio.create_task(items.merchantEvent.execute())
