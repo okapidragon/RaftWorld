8968fe67f50f0621ce04c1df8389dec937edecac
zacksloth5 <zackgourevitch@gmail.com>
2026-08-14 11:42:03 -0400

    Commited Bug for EZra

--- items.py
+++ items.py
@@ -501,3 +501,4 @@ salvageExplosives = task.Task(
 navalMinePopup2 = ev.Popup(text = "You swim down near the naval mine... (Explosives are RISKY)", optionTasks = [salvageExplosives, declineTask], duration = 7)
 
 navalMineEvent2 = ev.Event(name = "Naval Mine2", minCooldown = 0, screenPopup = navalMinePopup2, weightFunc = nullWeight)
+
