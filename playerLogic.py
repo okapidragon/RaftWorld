493cb61fe1928c407428668e3fe877a178240c13
zacksloth5 <zackgourevitch@gmail.com>
2026-08-14 16:42:44 -0400

    Fixed bug with no wood display

--- playerLogic.py
+++ playerLogic.py
@@ -157,7 +157,9 @@ class Boat:
 
         if not (inv.lookup("Wood") in imp.displayedResources):
             imp.displayedResources.append(inv.lookup("Wood"))
+            inv.inventoryUpdate()
             imp.asyncio.create_task(task.craftButtonUpdate())
+
 
     def decayFrame(self):
         if imp.boatDecay:
